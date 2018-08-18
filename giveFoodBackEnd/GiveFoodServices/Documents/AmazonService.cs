914f762d0fda3ec92531873730fc7b76ff531eb3
Sevdalina Pashova <spashova@dev.local>
2018-08-18 14:07:19 +0300

    fix dependecies

--- giveFoodBackEnd/GiveFoodServices/Documents/AmazonService.cs
+++ giveFoodBackEnd/GiveFoodServices/Documents/AmazonService.cs
@@ -1,6 +1,4 @@
-﻿using Amazon.S3;
-using Amazon.S3.Transfer;
-
+﻿
 namespace GiveFoodServices.Documents
 {
     public class AmazonService : IAmazonService
@@ -10,20 +8,20 @@ namespace GiveFoodServices.Documents
 
         public void UploadFile(string filePath)
         {
-            try
-            {
-                TransferUtility fileTransferUtility = new
-                    TransferUtility(new AmazonS3Client(Amazon.RegionEndpoint.EUCentral1));
+            //try
+            //{
+            //    TransferUtility fileTransferUtility = new
+            //        TransferUtility(new AmazonS3Client(Amazon.RegionEndpoint.EUCentral1));
 
-                // 1. Upload a file, file name is used as the object key name.
-                fileTransferUtility.Upload(filePath, BucketName);
+            //    // 1. Upload a file, file name is used as the object key name.
+            //    fileTransferUtility.Upload(filePath, BucketName);
 
                
-            }
-            catch (AmazonS3Exception s3Exception)
-            {
+            //}
+            //catch (AmazonS3Exception s3Exception)
+            //{
 
-            }
+          //  }
         }
         public void DownloadFile()
         {
